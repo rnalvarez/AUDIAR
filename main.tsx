dcf85025d7ad580e0ad8cf78b647f9b4a9a4e2e6
Ramiro N. Alvarez <64320107+rnalvarez@users.noreply.github.com>
2026-09-08 15:11:21 -0300

    Cargar estilos de créditos y licencias

--- main.tsx
+++ main.tsx
@@ -5,6 +5,7 @@ import "./style-tokens.css";
 import "./style-app.css";
 import "./style-layer-controls.css";
 import "./style-download-queue.css";
+import "./style-footer.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
